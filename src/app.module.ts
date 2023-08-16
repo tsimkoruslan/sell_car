898120f74d5dc7a03c6146b798c88faa24b12b0a
ruslantsimko <tsimkoruslan@gmail.com>
2023-08-17 01:16:43 +0300

    v 0.3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import {UserService} from "./user/user.service";
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {TypeOrmConfiguration} from "./config/database/type-orm-configuration";
 @Module({
-  imports: [UserModule],
+  imports: [
+      TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+      UserModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {ValidationPipe} from "@nestjs/common";
+import * as dotenv from 'dotenv';
+import * as process from "process";
+
+const environment = process.env.NODE_ENV ?? '';
+dotenv.config({path:`environments/${environment}.env`});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
